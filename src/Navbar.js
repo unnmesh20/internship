1a6162cae24fc1ad18cf5ad6381ea639a5ff7165
unnmesh20 <62304533+unnmesh20@users.noreply.github.com>
2021-02-26 21:35:44 +0530

    Add files via upload
    This project searches for user information based on input

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    
+    return (  
+        <nav className="navbar">
+            <h1>Welcome to React.js</h1>
+            <p><a href="/">Home</a></p>
+            <p><a href="/contact">Contact</a></p>
+        </nav>
+    );
+}
+ 
+export default Navbar;
